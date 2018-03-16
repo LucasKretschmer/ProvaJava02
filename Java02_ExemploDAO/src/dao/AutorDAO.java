096b644e9489f03818898a20e89cf32a562f9a2d
lukre <lukre@DESKTOP-OHBMRNV>
2018-03-15 21:01:42 -0300

    Aula Java 02

---
+++ Java02_ExemploDAO/src/dao/AutorDAO.java
@@ -0,0 +1,28 @@
+package dao;
+
+import java.util.ArrayList;
+import java.util.List;
+import model.Autor;
+
+public class AutorDAO {
+    
+    public void insert(){
+        
+    }
+    
+    public void update(){
+        
+    }
+    
+    public void delete(){
+        
+    }
+
+    public Autor findById(int id){
+        return new Autor();
+    }
+    
+    public List<Autor> findALL(){
+        return new ArrayList<>();
+    }
+}
